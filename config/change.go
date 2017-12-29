75e043c002875f8f7a228e73c7f28fa60a07d0c4
Mikaël Cluseau <mcluseau@isi.nc>
2017-12-29 19:37:52 +1100

    feat: custom backends flag

--- config/change.go
+++ config/change.go
@@ -2,11 +2,15 @@ package config
 
 import (
 	"flag"
+	"fmt"
+	"log"
+	"strings"
 	"time"
 )
 
 var (
 	changeApplyDelay = flag.Duration("change-apply-delay", 100*time.Millisecond, "Delay before applying change in Kubernetes configuration")
+	customBackends   = flag.String("custom", "", "Custom backend definitions (format: \"<host><path>:<target IP>:<target port>,...\")")
 
 	changeNum        uint64 = 0
 	appliedChangeNum uint64 = 0
@@ -35,6 +39,30 @@ func applyChange(myChangeNum uint64, callback NewConfigFunc) {
 	}
 
 	config := callback()
+
+	if len(*customBackends) != 0 {
+		for idx, be := range strings.Split(*customBackends, ",") {
+			parts := strings.Split(be, ":")
+
+			if len(parts) != 3 {
+				log.Fatal("bad custom backend format: %s", be)
+			}
+
+			hostParts := strings.SplitN(parts[0], "/", 2)
+			host := hostParts[0]
+			path := ""
+			if len(hostParts) > 1 {
+				path = "/" + hostParts[1]
+			}
+
+			target := parts[1] + ":" + parts[2]
+
+			config.HostBackends[host] = []*Backend{
+				NewBackend(fmt.Sprintf("custom[%d]", idx), path, target),
+			}
+		}
+	}
+
 	Current = &config
 
 	appliedChangeNum = changeNum
